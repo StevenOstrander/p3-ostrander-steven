a482c0fcd4a3cc7b9213e9023ed7ead0cb2c66ea
Josh Ostrander <joshostrand@ufl.edu>
2021-12-12 10:40:57 -0500

    Initial Commit - used HTML from assignment 5

---
+++ form/default.inc.php
@@ -0,0 +1,3 @@
+<div class="container">
+      <h1>Sorry something went wrong with your submission</h1>
+      <p>Please try again.</p>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Thank you for your enquiry</h1>
+    <p>Thanks for your interest! I have received your message, and will be in touch shortly.</p>
+</div>
